fffd3ab33b62af30556938040754614145baf5a7
Ethan Tong <ethantong.studio@gmail.com>
2020-03-26 21:39:05 -0500

    add strategy

--- backtest/trader.py
+++ backtest/trader.py
@@ -1,9 +1,10 @@
 import backtrader as bt
-from datetime import datetime 
+from datetime import datetime
+from strategy import SmaCross
 
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
-    
+    cerebro.addstrategy(SmaCross)
     data = bt.feeds.YahooFinanceData(dataname='ZOOM', fromdate=datetime(2018, 11, 30))
     cerebro.adddata(data)
     cerebro.run()
